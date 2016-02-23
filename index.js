479f5286127247376f611614edd9ef45f1f973cf
Espen Klem <espen.klem@gmail.com>
2016-02-23 09:41:33 +0100

    Updated API to latest search-index (0.7.6)

--- index.js
+++ index.js
@@ -4,30 +4,32 @@ var options = {
     indexPath: 'si-debug',
     logLevel: 'info',
     logSilent: false,
-    nGramLength: [1, 2]
 };
-var si = require('search-index')(options);
+var searchIndex = require('search-index')
 var jf = require('jsonfile');
 var configfile = '/Users/eklem/github_modules/search-index-indexer/config.json';
-var data = '/Users/eklem/github_modules/search-index-indexer/data-missing-field.json';
+var data = '/Users/eklem/github_modules/search-index-indexer/data.json';
 
 // Read config file
 var config = jf.readFileSync(configfile);
 var data = jf.readFileSync(data);
 
 // Add to search-index with si.add
-si.add(data, {
-  batchName: config.batchname,
-  fieldOptions: config.fieldOptions
-}, function (err) {
-  if (!err) {
-    console.log('Indexed!')
-    //console.dir(config)
-            
-    // Write config file to see what's there default    ### NOT WORKING ###
-    jf.writeFileSync(configfile, config, {spaces: 4})
- }
- else if (err) {
-     console.dir(err);
- }
+searchIndex(options, function(err, si) {
+    si.add(data, {
+        batchName: config.batchname,
+        fieldOptions: config.fieldOptions
+    }, function (err) {
+        if (!err) {
+            console.log('Indexed!')
+                //console.dir(config)
+
+            // Write config file to see what's there default    ### NOT WORKING ###
+            jf.writeFileSync(configfile, config, {
+                spaces: 4
+            })
+        } else if (err) {
+            console.dir(err);
+        }
+    });
 });
