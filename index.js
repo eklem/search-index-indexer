339b5a105acaf7a8a2601185e56a06af0ea55ef9
Espen Klem <espen.klem@gmail.com>
2016-03-31 15:32:48 +0200

    Working around issue #10 (upstream bug in search-index)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var options = {
     indexPath: 'si-debug',
-    logLevel: 'info',
+    logLevel: 'warn',
     logSilent: false,
 };
 var searchIndex = require('search-index');
@@ -8,14 +8,9 @@ var jf = require('jsonfile');
 var request = require('request');
 var program = require('commander');
 
-// Read config file
-// var config = jf.readFileSync(configfile);
-// var data = jf.readFileSync(data);
-
-
 // Taking arguments with 'commander' 
 program
-    .version('0.1.0')
+    .version('0.1.1')
     .option('-c, --config [url]', 'specify the url for the JSON config file. Just the flag -c will give you the default.', 'https://raw.githubusercontent.com/eklem/search-index-indexer/master/config.json')
     .option('-d, --data [url]', 'specify the url for the JSON data set. Just the flag -d will give you the default.', 'https://raw.githubusercontent.com/eklem/search-index-indexer/master/data.json')
     .parse(process.argv);
@@ -25,45 +20,44 @@ if (!process.argv.slice(2).length) {
     process.exit(1);
 }
 
-configurl = ('%s', program.config);
-dataurl = ('%s', program.data);
+// Assigning the input to variables
+var configurl = ('%s', program.config);
+var dataurl = ('%s', program.data);
 
 
-// Changing predefined 
+// Error message if variables not set for some reason
 if (!program.config) {
     console.log('no config');
-    //    program.config = config;
 };
 if (!program.data) {
     console.log('no data');
-    //    program.data = data;
 };
+
+// When bot inputs are present
+// (they always are because of the program.option setup with predefineds)
 if (program.config || program.data) {
-    //    console.log('\nLets do this: ' + config)
+    // Getting config file
     request(configurl, function (error, response, config) {
         if (!error && response.statusCode == 200) {
-            //          console.log(config);
+            // Getting data file
+            // I don't know how to work this, so doing async the stupid way
             request(dataurl, function (error, response, data) {
                 if (!error && response.statusCode == 200) {
-                    console.log(config);
-                    console.log(data);
                     
                     // Parse file content to JSON
                     config = JSON.parse(config);
                     data = JSON.parse(data);
 
-
                     //Add to search - index with si.add
-                    searchIndex(options, function (err, si) {
+                    searchIndex(options, function(err, si) {
                         si.add(data, {
                             batchName: config.batchname,
                             fieldOptions: config.fieldOptions
                         }, function (err) {
                             if (!err) {
                                 console.log('Indexed!')
-                                    //console.dir(config)
                             } else if (err) {
-                                console.dir(err);
+                                console.dir('something went wrong');
                             }
                         });
                     });
