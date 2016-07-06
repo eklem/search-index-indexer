b9b3ccdc6b798110cc1fe675d3e256540cc40b32
Espen Klem <espen.klem@gmail.com>
2016-07-06 16:07:41 +0200

    fixing nGramLength + calculating indexing time

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-var options = {
-    indexPath: 'si-debug',
-    logLevel: 'warn',
-    logSilent: false,
-};
-var searchIndex = require('search-index');
-var jf = require('jsonfile');
-var request = require('request');
-var program = require('commander');
+var searchIndex = require('search-index')
+var jf = require('jsonfile')
+var request = require('request')
+var program = require('commander')
 
 // Taking arguments with 'commander' 
 program
     .version('0.1.1')
     .option('-c, --config [url]', 'specify the url for the JSON config file. Just the flag -c will give you the default.', 'https://raw.githubusercontent.com/eklem/search-index-indexer/master/config.json')
     .option('-d, --data [url]', 'specify the url for the JSON data set. Just the flag -d will give you the default.', 'https://raw.githubusercontent.com/eklem/search-index-indexer/master/data.json')
-    .parse(process.argv);
+    .parse(process.argv)
 //Default displaying --help when no arguments
 if (!process.argv.slice(2).length) {
-    program.outputHelp();
-    process.exit(1);
+    program.outputHelp()
+    process.exit(1)
 }
 
 // Assigning the input to variables
-var configurl = ('%s', program.config);
-var dataurl = ('%s', program.data);
+var configurl = ('%s', program.config)
+var dataurl = ('%s', program.data)
 
 
 // Error message if variables not set for some reason
 if (!program.config) {
-    console.log('no config');
-};
+    console.log('no config')
+}
 if (!program.data) {
-    console.log('no data');
-};
+    console.log('no data')
+}
 
 // When bot inputs are present
 // (they always are because of the program.option setup with predefineds)
@@ -47,6 +42,16 @@ if (program.config || program.data) {
                     // Parse file content to JSON
                     config = JSON.parse(config);
                     data = JSON.parse(data);
+                    
+                    var options = {
+                        indexPath: 'si-debug',
+                        logLevel: 'info',
+                        logSilent: false,
+                        nGramLength: config.nGramLength
+                    }
+
+                    // Time before indexing
+                    var timeStart = Date.now();
 
                     //Add to search - index with si.add
                     searchIndex(options, function(err, si) {
@@ -55,7 +60,10 @@ if (program.config || program.data) {
                             fieldOptions: config.fieldOptions
                         }, function (err) {
                             if (!err) {
+                                var timeEnd = Date.now();
+                                var timeUsed = (timeEnd - timeStart) / 1000;
                                 console.log('Indexed!')
+                                console.log('in ' +  timeUsed + ' seconds')
                             } else if (err) {
                                 console.dir('something went wrong');
                             }
