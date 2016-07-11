43aca5910a2085b76f4138ed993cfd16a06c0423
Espen Klem <espen.klem@gmail.com>
2016-07-11 12:49:46 +0200

    Added start and end flags

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ program
   .version('0.1.2')
   .option('-c, --config [url]', 'specify the url for the JSON config file. Just the flag -c will give you the default.', 'https://raw.githubusercontent.com/eklem/search-index-indexer/master/config.json')
   .option('-d, --data [url]', 'specify the url for the JSON data set. Just the flag -d will give you the default.', 'https://raw.githubusercontent.com/eklem/search-index-indexer/master/data.json')
-  .option('-i, --items [number]', 'number of items to process each time. Just the flag -i will give you the default (10).',
-  10)
+  .option('-s, --start [number]', 'Which item in your array to start with. Defaults to 0',
+  0)
+  .option('-e, --end [number]', 'Which item in your array to end with. Defaults to length of array')
   .parse(process.argv)
 //Default displaying --help when no arguments
-if (!process.argv.slice(3).length) {
+if (!process.argv.slice(5).length) {
   program.outputHelp()
   process.exit(1)
 }
@@ -27,7 +28,9 @@ if (!process.argv.slice(3).length) {
 // Assigning the input to variables
 var configurl = ('%s', program.config)
 var dataurl = ('%s', program.data)
-var items = ('%s', program.items)
+var start = ('%s', program.start)
+var end = ('%s', program.end)
+
 
 // Error message if variables not set for some reason
 if (!program.config) {
@@ -41,10 +44,11 @@ if (!program.data) {
 // (They always are because of the program.option setup with predefineds)
 if (program.config && program.data) {
   console.log('have config and data url')
-  requestData(dataurl, options)
+  requestData(dataurl, options, start, end)
 }
 
-function requestData (dataurl, options) {
+// Getting data JSON
+function requestData (dataurl, options, start, end) {
   request(dataurl, function (error, response, data) {
     if (error) {
       console.log('Data request error for ' + dataurl + '\n' + error)
@@ -52,13 +56,13 @@ function requestData (dataurl, options) {
     if (!error && response.statusCode == 200) {
       // Parse file content to JSON
       data = JSON.parse(data)
-      console.log('data length: ' + data.length)
-      requestConfig(configurl, data, options)
+      requestConfig(configurl, data, options, start, end)
     }
   })
 }
 
-function requestConfig (configurl, data, options) {
+// Getting config JSON, setting nGramLength in options
+function requestConfig (configurl, data, options, start, end) {
   request(configurl, function (error, response, config) {
     if (error) {
       console.log('Config request error for ' + configurl + '\n' + error)
@@ -69,21 +73,29 @@ function requestConfig (configurl, data, options) {
       options.nGramLength = config.nGramLength
       console.log('config: ')
       console.dir(config)
-      indexData(config, data, options)
+      indexData(config, data, options, start, end)
      }
   })
 }
 
-function indexData (config, data, options) {
+
+// Indexer
+function indexData (config, data, options, start, end) {
   // Create a subset of data
-  console.log('options: ')
-  console.dir(options)
-  var dataSubset = data.slice(0,items)
-  //console.dir(dataSubset)
+  if (!end) {
+    var end = data.length + 1
+  }
+  if (end <= data.length) {
+    end = end + 1
+  }
+  if (end > data.lengt) {
+    end = data.length +1
+  }
+  var dataSubset = data.slice(start, end)
   data = null
-  // Time before indexing
-  var timeStart = Date.now()  
+
   //Add to search - index with si.add
+  var timeStart = Date.now()  
   searchIndex(options, function(err, si) {
     si.add(dataSubset, {
       batchName: config.batchname,
@@ -91,6 +103,7 @@ function indexData (config, data, options) {
     }, function (err) {
       if (!err) {
         var timeEnd = Date.now()
+        console.log('Ending indexing at:   ' + timeEnd)
         var timeUsed = (timeEnd - timeStart) / 1000;
         console.log('Indexed!')
         console.log('in ' +  timeUsed + ' seconds')
