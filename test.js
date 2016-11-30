016c51eeea5d198530a4b3e823c650540b48daba
Espen Klem <espen.klem@gmail.com>
2016-11-30 06:41:12 -0500

    Upgrading to 'search-index' v0.9.x support

--- test.js
+++ test.js
@@ -2,8 +2,7 @@ const JSONStream = require('JSONStream')
 const chalk = require('chalk')
 const request = require('request')
 const tc = require('term-cluster')
-//const url = 'https://raw.githubusercontent.com/eklem/dataset-vinmonopolet/master/dataset-vinmonopolet-test.str'
-var program = require('commander')
+const url = 'https://raw.githubusercontent.com/eklem/dataset-vinmonopolet/master/dataset-vinmonopolet-test.str'
 
 const ops = {
   indexPath: 'myCoolIndex',
@@ -11,26 +10,11 @@ const ops = {
 }
 
 var index
-// Taking arguments with 'commander'
-program
-  .version('0.2.0')
-  .option('-c, --config [url]', 'specify the url for the JSON config file. Just the flag -c will give you the default.', 'https://raw.githubusercontent.com/eklem/search-index-indexer/master/config.json')
-  .option('-d, --data [url]', 'specify the url for the JSON data set. Just the flag -d will give you the default.', 'https://raw.githubusercontent.com/eklem/dataset-vinmonopolet/master/dataset-vinmonopolet-test.str')
-  .parse(process.argv)
-//Default displaying --help when no arguments
-if (!process.argv.slice(1).length) {
-  program.outputHelp()
-  process.exit(1)
-}
-
-// Assigning the input to variables
-//var configurl = ('%s', program.config)
-var dataurl = ('%s', program.data)
 
 const indexData = function(err, newIndex) {
   if (!err) {
     index = newIndex
-    request(dataurl)
+    request(url)
       .pipe(JSONStream.parse())
       .pipe(index.defaultPipeline())
       .pipe(index.add())
